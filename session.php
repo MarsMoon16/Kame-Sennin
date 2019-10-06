fc2883c45e8c6314bd2afb68b957ad8cfa48db3d
MarsMoon544 <34398758+MarsMoon16@users.noreply.github.com>
2019-10-06 23:35:06 +0200

    Update session.php

--- session.php
+++ session.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Kame Sennin</title>
+<meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE" />
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div class="top">
+<h1>Session</h1>
 
+<div class="content">
+<a href="index.php"><button>Done</button></a>
+</div>
+
+
+  <!-- Bottom -->
+<div class="bottom">
+<p> Votre navigateur : <?php echo $_SERVER['HTTP_USER_AGENT']; ?>
+ ------ Votre adresse IP :
+<?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+<p><?php echo "nous sommes le " . date("d/m/Y"); 
+echo ". il est ". date("h:ia");  ?></p>
+</div>
+</body>
+</html>
